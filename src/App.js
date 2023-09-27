30a5cfaf0e8339f6663fa1916070f7d1ed095833
ewan-porter <69685987+ewan-porter@users.noreply.github.com>
2023-09-27 21:17:10 +0100

    'help'

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [serverList, setServerList] = useState([]);
+  const apiKey = "B27121BA1951A368156793847434CDA3";
+  const apiUrl = `https://api.steampowered.com/IGameServersService/GetServerList/v1/?key=${apiKey}&limit=40000&filter=\\appid\\730\\name_match\\Valve%20CS:GO*Server*`;
+  useEffect(() => {
+    // Make a GET request to the Steam API
+    fetch(apiUrl, {
+      method: "GET",
+      headers: {
+        "access-control-allow-origin": "*",
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        // Handle the data here
+        setServerList(data.response.servers);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
+
+  console.log(serverList);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>{serverList}</div>
       </header>
     </div>
   );
